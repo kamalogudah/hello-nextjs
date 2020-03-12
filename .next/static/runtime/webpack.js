5093bc9a07341992ed465130a8d76571048afd39
kamalogudah <mcpaul2058@gmail.com>
2020-03-12 14:01:02 +0300

    Passing Data via Query Strings

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "91d75272c6337fd5c96e";
+/******/ 	var hotCurrentHash = "54245b72de83fc5fc1e0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
